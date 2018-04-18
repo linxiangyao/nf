792df235fac9de09a12568f4a507151fd45d5b21
ryanlin <ryanlin@tencent.com>
2018-04-18 15:09:30 +0800

    clear.bat and ignore list

--- clear.bat
+++ clear.bat
@@ -3,14 +3,19 @@ rmdir project\win\nf\.vs /q/s
 rmdir project\win\nf\x64 /q/s
 rmdir project\win\nf\Debug /q/s
 rmdir project\win\nf\Release /q/s
+del   project\win\nf\*.log /q/s
+del   project\win\nf\*.db /q/s
+
 
 rmdir project\win\nf\nfClient\x64 /q/s
 rmdir project\win\nf\nfClient\Debug /q/s
 rmdir project\win\nf\nfClient\Release /q/s
+del   project\win\nf\nfClient\*.log /q/s
+del   project\win\nf\nfClient\*.db /q/s
+
 
 rmdir project\win\nf\nfServer\x64 /q/s
 rmdir project\win\nf\nfServer\Debug /q/s
 rmdir project\win\nf\nfServer\Release /q/s
-
-del project\win\nf\*.log /q/s
-del project\win\nf\*.db /q/s
+del   project\win\nf\nfServer\*.log /q/s
+del   project\win\nf\nfServer\*.db /q/s
